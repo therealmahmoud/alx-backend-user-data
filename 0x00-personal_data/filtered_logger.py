98f36da72e5870d12771cf9f6c00b94b51bc5fb4
therealmahmoud <moha91875460@gmail.com>
2024-08-01 08:37:37 +0300

    Updated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Task 0."""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List,
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
+    """ Returns the log message obfuscated. """
+    pattern = '|'.join([f'{field}=.*?{separator}' for field in fields])
+    return re.sub(pattern, lambda x: x.group().split('=')[0] + '=' +
+                  redaction + separator, message)
+
