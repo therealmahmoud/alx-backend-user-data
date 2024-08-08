e188883feb70e04853ef0bea0ddc7d22baf58b0d
therealmahmoud <moha91875460@gmail.com>
2024-08-08 14:03:13 +0300

    Updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,5 +23,6 @@ class SessionAuth(Auth):
             return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
+        """Retrieves the user associated with the request."""
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
