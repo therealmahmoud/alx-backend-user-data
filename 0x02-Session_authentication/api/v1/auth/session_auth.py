1690e4df05d5d9b10b8c5bc5e07f3813256634f0
therealmahmoud <moha91875460@gmail.com>
2024-08-08 13:27:26 +0300

    Updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,6 +17,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user id of the user"""
         if not session_id or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
