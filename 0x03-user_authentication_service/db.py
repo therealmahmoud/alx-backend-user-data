dc12522d9bc78d0ef1131436240f9afe96e2a07d
therealmahmoud <moha91875460@gmail.com>
2024-08-13 08:12:20 +0300

    Updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,6 +32,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         if not email or not hashed_password:
+            self._session.rollback()
             user = None
         elif email and hashed_password:
             user = User(email=email, hashed_password=hashed_password)
