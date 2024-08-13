94176c38ddff57e9180697c536540c92a70b9edd
therealmahmoud <moha91875460@gmail.com>
2024-08-13 14:04:09 +0300

    Updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
@@ -12,11 +11,11 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -25,7 +24,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
