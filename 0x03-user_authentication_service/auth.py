45660b6717d4c8632d3a7ad1d5471bc3a9d1b490
therealmahmoud <moha91875460@gmail.com>
2024-08-14 11:26:12 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,14 +17,14 @@ class Auth:
     """
 
     def __init__(self):
-        """Initialize a new instance."""
+        """Initializes a new Auth instance.
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Adds a new user to the database.
-        """
+        """ Register a new user."""
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        raise ValueError("User {} already exists".format(email))
+        raise ValueError(f'User {email} already exists')
