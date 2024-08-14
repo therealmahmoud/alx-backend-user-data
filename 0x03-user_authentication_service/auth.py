e94ced517ee57e009a79ebd2c608d6995c86e6e9
therealmahmoud <moha91875460@gmail.com>
2024-08-14 10:00:44 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.exc import NoResultFound
 
+
 def _hash_password(password: str) -> bytes:
     """ Hashing and encrypting the password"""
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
