594660537bfac04033b19bbac75a00ab760e25bd
therealmahmoud <moha91875460@gmail.com>
2024-08-14 09:00:01 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ The authentcation module."""
 import bcrypt
-import base64
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +8,16 @@ def _hash_password(password: str) -> bytes:
     encoded = password.encode('utf8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(encoded, salt)
+
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user():
+        pass
