464fc69d6b88007f746e22adb8287cc197f3a5b3
therealmahmoud <moha91875460@gmail.com>
2024-08-14 08:24:01 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,5 +4,6 @@ import bcrypt
 import base64
 
 def _hash_password(password: str) -> bytes:
+    """ Hashing and encrypting the password"""
     hashed_pass = bcrypt.hashpw(base64.b64encode(password), bcrypt.gensalt())
     return hashed_pass
