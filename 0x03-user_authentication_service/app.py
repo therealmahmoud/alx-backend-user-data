0d622d949a637f88ac431ee4accca91b0e0a9acd
therealmahmoud <moha91875460@gmail.com>
2024-08-15 15:58:52 +0300

    Updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,7 +56,7 @@ def profile() -> str:
     if not user:
         abort(403)
     return jsonify({"email": user.email}), 200
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
