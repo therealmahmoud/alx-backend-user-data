d760947f1734ffe2f25b496cc85b9c9d08aa9ee7
therealmahmoud <moha91875460@gmail.com>
2024-08-15 20:26:22 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,15 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        new_pass = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=new_pass,
+                             reset_token=None)
