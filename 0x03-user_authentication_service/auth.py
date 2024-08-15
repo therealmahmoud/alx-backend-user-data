c44c155f8abec2419305e4f56fa6ce12361230e8
therealmahmoud <moha91875460@gmail.com>
2024-08-15 20:27:20 +0300

    Updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,6 +88,7 @@ class Auth:
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password. """
         user = None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
