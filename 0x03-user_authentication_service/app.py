105ebec92699a8026737ad158ed4fbc438c90e6e
therealmahmoud <moha91875460@gmail.com>
2024-08-15 17:42:54 +0300

    Updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,5 +58,15 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    email = request.form.get("email")
+    if AUTH._db.find_user_by(email=email):
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
