564fc258b3ef343e5252d57f87efa9bfcfef9148
therealmahmoud <moha91875460@gmail.com>
2024-08-15 13:58:11 +0300

    Updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,5 +25,16 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/session", methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ Account login."""
+    email, password = request.form.get('email'), request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    return jsonify({"email": email, "message": "logged in"}).\
+        set_cookie("session_id", session_id)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
